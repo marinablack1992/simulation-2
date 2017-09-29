2e3ea866ced22ed69b0351b33aa8054c1b04d5f6
Cory Barton <coryjbarton@gmail.com>
2017-09-29 13:22:43 -0600

    Wizard 5 step

---
+++ sim-2/src/Wizard/StepFive/StepFive.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { updateDesiredRent, updateWizard } from '';
+
+class StepFive extends Component {
+    render(){
+        const { updateDesiredRent, recommendedRent, updateWizard } = this.props;
+
+        return(
+            <div className="step_container">
+                <p> Step 5</p>
+            <div className="step5_container">
+                <div className="step5_recommended_rent">
+                    <span>Recommended Rent: ${recommendedRent}</span><br />
+                </div>
+                <div className="step5_desired_rent">
+                    <span>Desired Rent</span><br/>
+                    <input placeholder="desiredRent" type="text" onChange={ ( e ) => updateDesiredRent( e.target.value ) } />   
+                </div>
+            <Link to="/StepFour"><button className="/StepFive"> Previous Step </button></Link>
+            <Link to="/"><button className="complete" onClick={() => console.log(this.props) }> Complete </button></Link>
+        </div>
+      </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+
+return state;
+}
+
+export default connect (mapStateToProps, { updateDesiredRent, updateWizard }) (StepFive);
---
+++ sim-2/src/Wizard/StepTwo/StepTwo.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { updatePropAddress, updatePropCity, updatePropState, updatePropZip } from '';
+
+class StepTwo extends Component {
+    render(){
+        const { updatePropAddress, updatePropCity, updatePropState, updatePropZip } = this.props;
+
+        return(
+        <div className="step_container">
+                <p> Step 2</p>
+            <div className="step2_address_container">
+                <span>Address</span>
+                <input placeholder="address" type="text" onChange={ ( e ) => updatePropAddress( e.target.value ) } />
+            </div>
+            
+            <div className="step2_middle_container">
+                <div className="step2_city">
+                    <span>City</span>
+                    <input placeholder="City" type="text" onChange={ ( e ) => updatePropCity( e.target.value ) } />   
+                </div>
+                <div className="step2_state">
+                    <span>State</span>
+                    <input placeholder="State" type="text" onChange={ ( e ) => updatePropState( e.target.value ) } /> 
+                </div> 
+            </div>
+
+            <div className="step2_zip_container">
+                <span>Zip</span>
+                <input placeholder="Zip" type="text" onChange={ ( e ) => updatePropZip( e.target.value ) } />
+ 
+        <Link to="/StepOne"><button className="/StepTwo"> Previous Step </button></Link>
+        <Link to="/StepThree"><button className="/StepTwo"> Next Step </button></Link>
+            </div>
+        </div>          
+      
+
+       )
+    }
+}
+
+function mapStateToProps(st) {
+    const { address, city, state, zip } = st;
+
+    return{
+        address,
+        city,
+        state,
+        zip
+    };
+}
+
+export default connect ( mapStateToProps, { updatePropAddress, updatePropCity, updatePropState, updatePropZip } ) ( StepTwo );
