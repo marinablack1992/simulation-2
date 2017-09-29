23449a4b0524e2900c356c030c95ced508c756e4
Cory Barton <coryjbarton@gmail.com>
2017-09-29 14:51:57 -0600

    Added reducer to Steps 1-5

--- sim-2/src/Wizard/StepFive/StepFive.js
+++ sim-2/src/Wizard/StepFive/StepFive.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 import { connect } from 'react-redux';
-import { updateDesiredRent, updateWizard } from '';
+import { updateDesiredRent, updateWizard } from '../../ducks/reducer';
 
 class StepFive extends Component {
     render(){
